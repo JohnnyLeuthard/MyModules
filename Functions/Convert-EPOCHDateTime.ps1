31e88c7e5a8042abde13c5f1bc5e64807629454d
JohnnyLeuthard <drack69+github@gmail.com>
2018-12-11 08:19:15 -0500

    Updated Convert-EPOCHDateTime.ps1 to allow pipeline input and take multiple inputs.

--- Functions/Convert-EPOCHDateTime.ps1
+++ Functions/Convert-EPOCHDateTime.ps1
@@ -1,26 +1,37 @@
 Function Convert-EPOCHDateTime
 {
+  [CmdletBinding()]
   Param
   (
-    $InputTime = '153242218'
+    [Parameter(Position = 0, ValueFromPipeline = $true, Mandatory = $True)]
+    $InputTime
   )
-
-  $FriendlyDate = (get-date '1/1/1970').addseconds($inputTime)
-
-  # Get Local timezone
-  ##$strCurrentTimeZone = (gwm-wmiobject win32_timezone).StandardName
-  ##$TZ = [System.TimeZoneInfo]::FindSystemTimeZoneById(($strCurrentTimeZone)
-  ##$LocalTime = [System.TimeZoneInfo]::convertTimeZone.tolocalTime($FriendlyDate) )
-  $LocalTime = ( [System.TimeZone]::CurrentTimeZone.ToLocalTime($FriendlyDate) )
-
-  $HashData = [ordered]@{
-    'InputTime'     = $InputTime
-    'LocalTimezone' = ( [System.TimeZone]::CurrentTimeZone).StandardName
-    'LocalTime'     = $LocalTime
-    'UTCTime'       = $FriendlyDate
+  Begin
+  {
   }
-  New-Object -TypeName psobject -Property $HashData
+  Process
+  {
 
+    $FriendlyDate = (get-date '1/1/1970').addseconds($inputTime)
+
+    # Get Local timezone
+    ##$strCurrentTimeZone = (gwm-wmiobject win32_timezone).StandardName
+    ##$TZ = [System.TimeZoneInfo]::FindSystemTimeZoneById(($strCurrentTimeZone)
+    ##$LocalTime = [System.TimeZoneInfo]::convertTimeZone.tolocalTime($FriendlyDate) )
+    $LocalTime = ( [System.TimeZone]::CurrentTimeZone.ToLocalTime($FriendlyDate) )
+
+    $HashData = [ordered]@{
+      'InputTime'     = $InputTime
+      'LocalTimezone' = ( [System.TimeZone]::CurrentTimeZone).StandardName
+      'LocalTime'     = $LocalTime
+      'UTCTime'       = $FriendlyDate
+    }
+    New-Object -TypeName psobject -Property $HashData
+  }
+  end
+  {
+
+  }
 }#(Function Convert-EPOCHDateTime)
 ##################
 <#
