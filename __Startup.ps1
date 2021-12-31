a8f5f92c38946f58a760d1cda80a1e56dca093b2
JohnnyLeuthard <drack69@gmail.com>
2021-12-31 12:09:10 -0500

    updating, cleaning up, etc.

--- __Startup.ps1
+++ __Startup.ps1
@@ -1,4 +1,9 @@
-$requiredFiles = (Get-Module MyModule -ListAvailable).FileList
+
+
+##?? pdte this to be dynamic
+$ModuleName = 'MyModules'
+
+$requiredFiles = (Get-Module $ModuleName -ListAvailable).FileList
 $requiredFiles | foreach {
     if (-not (Test-Path -Path $_ -PathType Leaf)) {
         Write-Warning "The file [$($_)] does not exist and is required for this module."       
