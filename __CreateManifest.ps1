d73bb37db62a28122196c16aeb558d3297930b2e
JohnnyLeuthard <drack69@gmail.com>
2021-12-31 12:24:11 -0500

    bla bla bla

--- __CreateManifest.ps1
+++ __CreateManifest.ps1
@@ -1,32 +1,31 @@
 ﻿
-$ModuleName = 'MyModules'
+$ModuleName = 'MyModules'  #?? et this dynamiclly 
 $ModPath = "C:\Users\Administrator\Documents\WindowsPowerShell\Modules\$ModuleName\$ModuleName.psd1"
     
 $Hash = @{
 
-    Path = $ModPath
-    RootModule = 'MyModules.psm1'
-    ModuleVersion = '1.0'
-    Guid ='33316de0-fd66-403b-ae96-11a211b082eb' 
-    Author ='Johnny Leuthard' 
-    CompanyName = 'Johnny Leuthard'
-    Copyright = '(c) 2018 johnnyleuthard. All rights reserved.'
-    Description = 'My custom modules. testing, playing, templates, etc.'
-    PowerShellVersion = '5.1'
-    #PowerShellHostName = ''
-    #PowerShellHostVersion = ''
-    #DotNetFrameworkVersion = ''
-    #CLRVersion = ''
-    #RequiredAssemblies = ''
-    ScriptsToProcess = '__Startup.ps1'
-    FunctionsToExport = '*'
-    CmdletsToExport = '*'
-    VariablesToExport ='*'
-    AliasesToExport ='*'
-    FileList = ('RequiredFile1.txt','RequiredFile2.txt')
-    HelpInfoURI = 'https://github.com/JohnnyLeuthard/MyModulesModHelp'
+    'Path'                      = $ModPath
+    'RootModule'                = 'MyModules.psm1'
+    'ModuleVersion'             = '1.0'
+    'Guid'                      ='33316de0-fd66-403b-ae96-11a211b082eb' 
+    'Author'                    ='Johnny Leuthard' 
+    'CompanyName'               = 'Johnny Leuthard'
+    'Copyright'                 = '(c) 2018 johnnyleuthard. All rights reserved.'
+    'Description'               = 'My custom modules. testing, playing, templates, etc.'
+    'PowerShellVersion'         = '5.1'
+    #'PowerShellHostName'       = ''
+    #'PowerShellHostVersion'    = ''
+    #'DotNetFrameworkVersion'   = ''
+    #'CLRVersion'               = ''
+    #'RequiredAssemblies'       = ''
+    'ScriptsToProcess'          = '__Startup.ps1'
+    'FunctionsToExport'         = '*'
+    'CmdletsToExport'           = '*'
+    'VariablesToExport'         ='*'
+    'AliasesToExport'           ='*'
+    'FileList'                  = ('RequiredFile1.txt','RequiredFile2.txt')
+    'HelpInfoURI'               = 'https://github.com/JohnnyLeuthard/MyModulesModHelp'
 }
 
 New-ModuleManifest @hash
-
 
