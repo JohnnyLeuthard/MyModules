3dac61fa7680769e81e28f738d718ae6fde78614
JohnnyLeuthard <drack69@gmail.com>
2024-06-28 12:55:54 -0400

    Updating files

--- MyModules.psm1
+++ MyModules.psm1
@@ -1,25 +1,30 @@
-
 # Set a Global variable (used for testing, validation, etc.)
 Set-Variable -Name TestGlobalVar -Value "This is a test 1...2...3" -Scope Global
 
-#-- Dynamicly determine Module Name
-$directorySeparator             = [System.IO.Path]::DirectorySeparatorChar
-$moduleName                     = $PSScriptRoot.Split($directorySeparator)[-1]
+#write-Host "PSScriptRoot path is: $PSScriptRoot" -ForegroundColor Green
+# Dynamicly determine Module Name
+$directorySeparator = [System.IO.Path]::DirectorySeparatorChar
+$moduleName = $PSScriptRoot.Split($directorySeparator)[-1]
 #-- Manifest file name
-$moduleManifest                 = $PSScriptRoot + $directorySeparator + $moduleName + '.psd1'
+$moduleManifest = $PSScriptRoot + $directorySeparator + $moduleName + '.psd1'
 #-- Public functions folder path
-$GLOBAL:publicFunctionsPath     = $PSScriptRoot + $directorySeparator + 'Public' + $directorySeparator + 'Functions'
+$publicFunctionsPath    = $PSScriptRoot + $directorySeparator + 'Public' + $directorySeparator + 'Functions'
 #-- Private functions folder path
-$privateFunctionsPath           = $PSScriptRoot + $directorySeparator + 'Private' + $directorySeparator + 'Functions'
-$currentManifest                = Test-ModuleManifest $moduleManifest
+$privateFunctionsPath   = $PSScriptRoot + $directorySeparator + 'Private' + $directorySeparator + 'Functions'
+$currentManifest        = Test-ModuleManifest $moduleManifest
 
-$aliases = @()
-$Global:publicFunctions = Get-ChildItem -Path $GLOBAL:publicFunctionsPath -Recurse | Where-Object { ($_Name -notlike "__*") -and ($_.Extension -eq '.ps1')}
-$privateFunctions       = Get-ChildItem -Path $privateFunctionsPath -Recurse | Where-Object { ($_Name -notlike "__*") -and ($_.Extension -eq '.ps1')}
-$Global:publicFunctions | ForEach-Object { . $_.FullName }
+# Get list of PS1 files in the functions folders (files atrating with __ get ignored)
+$aliases = @()  ##??
+$publicFunctions  = Get-ChildItem -Path $publicFunctionsPath -Recurse  | Where-Object { ($_.Name -notlike "__*") -and ($_.Extension -eq '.ps1')}
+$privateFunctions = Get-ChildItem -Path $privateFunctionsPath -Recurse | Where-Object { ($_.Name -notlike "__*") -and ($_.Extension -eq '.ps1')}
+#$publicFunctions  = Get-ChildItem -Path $publicFunctionsPath  | Where-Object {$_.Extension -eq '.ps1'}
+#$privateFunctions = Get-ChildItem -Path $privateFunctionsPath | Where-Object {$_.Extension -eq '.ps1'}
+#-- DOT source scripts
+$PublicFunctions | ForEach-Object { . $_.FullName }
 $privateFunctions | ForEach-Object { . $_.FullName }
 
-$Global:publicFunctions | ForEach-Object { # Export all of the public functions from this module
+# Export all of the public functions from this module
+$publicFunctions | ForEach-Object { 
 
     # The command has already been sourced in above. Query any defined aliases.
     $alias = Get-Alias -Definition $_.BaseName -ErrorAction SilentlyContinue
@@ -32,23 +37,33 @@ $Global:publicFunctions | ForEach-Object { # Export all of the public functions 
     }
 }
 
-$functionsAdded     = $Global:publicFunctions | Where-Object {$_.BaseName -notin $currentManifest.ExportedFunctions.Keys}
-$functionsRemoved   = $currentManifest.ExportedFunctions.Keys | Where-Object {$_ -notin $Global:publicFunctions.BaseName}
+# Get list of ublic functions/alises losted in manifest and add/remove changes
+$functionsAdded     = $publicFunctions | Where-Object {$_.BaseName -notin $currentManifest.ExportedFunctions.Keys}
+$functionsRemoved   = $currentManifest.ExportedFunctions.Keys | Where-Object {$_ -notin $publicFunctions.BaseName}
 $aliasesAdded       = $aliases | Where-Object {$_ -notin $currentManifest.ExportedAliases.Keys}
 $aliasesRemoved     = $currentManifest.ExportedAliases.Keys | Where-Object {$_ -notin $aliases}
 
+# If there are aliases or public functions added/removed update the manifest with those changes
 if ($functionsAdded -or $functionsRemoved -or $aliasesAdded -or $aliasesRemoved) {
     try {
         $updateModuleManifestParams = @{}
         $updateModuleManifestParams.Add('Path', $moduleManifest)
         $updateModuleManifestParams.Add('ErrorAction', 'Stop')
         if ($aliases.Count -gt 0) { $updateModuleManifestParams.Add('AliasesToExport', $aliases) }
-        if ($GLOBAL:publicFunctions.Count -gt 0) { $updateModuleManifestParams.Add('FunctionsToExport', $GLOBAL:publicFunctions.BaseName) }
-
+        if ($publicFunctions.Count -gt 0) { $updateModuleManifestParams.Add('FunctionsToExport', $publicFunctions.BaseName) }
+        # Update manifest (PSD1) file
         Update-ModuleManifest @updateModuleManifestParams
     }
     catch {
         $_ | Write-Error
     }
 }
+#########################
+### NOTES
+#########################
+<#
+
+
+
+#>
 
--- en-us/MD/Get-SPN.md
+++ en-us/MD/Get-SPN.md
@@ -33,21 +33,22 @@ Gets the SPN of the current computer
 Get-SPN Server1
 Get-SPN -ComputerName Server1
 ```
-Gets the SPN's of a server names Server1
 
+Gets the SPN's of a server names Server1
 
 ### EXAMPLE 3
 ```powershell
 $ServerList | Get-SPN
 ```
+
 Gets the SPN's of a list of servers contained in the variable names $ServerList
 
 ### EXAMPLE 4
 ```powershell
 Get-Content "C:\Temp\Servers.txt" | Get-SPN
 ```
-Gets the SPN's of a list of servers contained in the text file C:\Temp\Servers.txt
 
+Gets the SPN's of a list of servers contained in the text file C:\Temp\Servers.txt
 
 ### EXAMPLE 5
 ```powershell
@@ -61,6 +62,7 @@ Then exports it to the CSV file C:\Temp\SPNData.csv
 ```powershell
 Get-Content "C:\Temp\Servers.txt" | Get-SPN | Export-Clixml  "C:\Temp\SPNData.xml"
 ```
+
 Gets the SPN's of a list of servers contained in the text file C:\Temp\Servers.txt 
 Then exports it to the XML file C:\Temp\SPNData.xml
 
